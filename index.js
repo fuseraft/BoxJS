9e7e19b7a6f1b0baa21303beb7ee6562b180bceb
Scott Christopher Stauffer <scstauf@gmail.com>
2018-12-31 22:08:13 -0600

    asdf

--- index.js
+++ index.js
@@ -5,35 +5,46 @@ const box = require('./box');
 const app = express();
 const port = process.env.PORT || 3000;
 const readFile = (path) => { try { return fs.readFileSync(path, 'utf8'); } catch (ignore) { return ''; } };
+const bodyHtml = readFile(path.join(__dirname, 'box/body.html'));
+const headHtml = readFile(path.join(__dirname, 'box/head.html'));
 
 const buildPage = (title, content) => {
+    let test = '';
+    let files = [];
+    
+    fs.readdirSync(__dirname).forEach(file => {
+        files.push(file);
+    })
+    
+    test = files.join('\n');
+
     let config = {
         title: title,
         styles: [
             '//fonts.googleapis.com/css?family=Raleway:400,300,600',
             'https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css',
             'https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css',
-            'css/custom.css',
-            'css/prism.css'
+            'box/css/custom.css',
+            'box/css/prism.css'
         ],
         scripts: [
-            'prism.js'
+            'box/prism.js'
         ],
         body: {
             tag: 'body',
-            content: readFile(path.join(__dirname, 'box/body.html')),
+            content: bodyHtml,
             after: {
                 tag: 'pre',
                 children: [{ 
                     tag: 'code', 
-                    content: readFile(path.join(__dirname, 'index.js')), 
+                    content: test,//readFile(path.join(__dirname, 'index.js')), 
                     classList: ['language-javascript'] 
                 }]
             }
         },
         head: {
             tag: 'head',
-            content: readFile(path.join(__dirname, 'box/head.html'))
+            content: headHtml
         },
         injections: {
             'btnDownload': new box({
@@ -57,8 +68,9 @@ const buildPage = (title, content) => {
 app.get('/', (req, res) => { res.send(buildPage('BoxJS: A boxy HTML generator.')); });
 app.get('/download', (req, res) => { 
     res.set('Content-Type', 'application/javascript');
-    res.download('box.js');
+    res.download(path.join(__dirname, 'box.js'));
 });
-app.get('/code', (req, res) => { res.sendFile('box.js')});
-app.use(express.static('/box'));
+app.get('/code', (req, res) => { res.sendFile('box.js', { root: __dirname })});
+app.use('/box', express.static(path.join(__dirname, 'box/')));
+app.use('/box/css', express.static(path.join(__dirname, 'box/css/')));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
